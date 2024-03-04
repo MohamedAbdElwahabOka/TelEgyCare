b4f82e014dbc692b4e65fdeabfd7e0195d33ccdf
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-03-04 20:55:21 +0200

    Update API URL

--- app/_utils/axiosClient.js
+++ app/_utils/axiosClient.js
@@ -1,7 +1,8 @@
 const { default: axios } = require('axios')
 const apikey = process.env.NEXT_PUBLIC_REST_API_KEY;
 
-const apiUrl = 'http://localhost:1337/api'
+// const apiUrl = 'http://localhost:1337/api'
+const apiUrl = 'https://telegycare-s.onrender.com/api'
 
 const axiosClient = axios.create({
     baseURL: apiUrl,

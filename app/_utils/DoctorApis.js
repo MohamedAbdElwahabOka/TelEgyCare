8d351ef2bb32d4e217631bd73208b7f6b5a6514c
MohamedAbdElwahabOka <boka22986@gmail.com>
2024-05-03 07:44:50 +0300

    "Update SignUp page with bcryptjs for password hashing and sweetalert2 for better user feedback"
    This commit updates the SignUp page to include bcryptjs for password hashing and sweetalert2 for better user feedback. The

--- app/_utils/DoctorApis.js
+++ app/_utils/DoctorApis.js
@@ -4,10 +4,10 @@ const getDoctor = () => axiosClient.get('/doctors?populate=*');
 
 const getDoctorByRegNum = (regNum) => axiosClient.get(`/Doctors?filters[reg_Num][$eq]=${regNum}&populate=*`);
 
-
-
+const addDoctor = (doctor) => axiosClient.post('/doctors', doctor);
 
 export default {
     getDoctor,
-    getDoctorByRegNum
+    getDoctorByRegNum,
+    addDoctor
 }
